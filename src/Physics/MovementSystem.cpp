21aef622eb089a043090c82d887108b14e169ff5
JulietteL <juliette.lagrange@sfr.fr>
2014-05-20 09:57:53 +0200

    Add movement system

---
+++ src/Physics/MovementSystem.cpp
@@ -0,0 +1,31 @@
+#include "MovementSystem.h"
+
+#include "MovementComponent.h"
+#include "../Geometry/PositionComponent.h"
+
+using Geometry::PositionComponent;
+
+namespace Physics
+{
+  void MovementSystem::run()
+  {
+    World& world = getWorld();
+
+    // Get all the entities with movement and position components
+    Ecs::ComponentGroup::ComponentTypeCollection types;
+    types.insert(MovementComponent::Type);
+    types.insert(PositionComponent::Type);
+
+    Ecs::ComponentGroup prototype(types);
+    Ecs::World::ComponentGroupCollection groups = world.getComponents(prototype);
+
+    // Update their positions
+    Ecs::World::ComponentGroupCollection::iterator group;
+    for (group = groups.begin(); group != groups.end(); ++group)
+      {
+        PositionComponent & positionComponent = static_cast<PositionComponent &>(group->getComponent(PositionComponent::Type));
+        MovementComponent & movementComponent = static_cast<MovementComponent &>(group->getComponent(MovementComponent::Type));
+        positionComponent.setPosition(positionComponent.getPosition() + movementComponent.getVelocity());
+      }
+  }
+}
