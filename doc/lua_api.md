ce18a2805f262234814a5f7b267e28c3f82fec48
Steven Rémot <steven.remot@gmail.com>
2015-09-13 18:34:23 +0200

    Create Lua API for Perlin noise

--- doc/lua_api.md
+++ doc/lua_api.md
@@ -22,10 +22,29 @@ If no seed is provided, it uses the world seed.
 Avoid injecting your own seeds for level generation, except if you
 merge it with the world's one.
 
-#### `tlsd.number_generator:get_uniform(min: number, max: number) -> number`
+#### `number_generator:get_uniform(min: number, max: number) -> number`
 
 Return a random number between `min` and `max`, following a uniform distribution.
 
-#### `tlsd.number_generator:get_poisson(mean: integer > 0) -> integer > 0`
+#### `number_generator:get_poisson(mean: integer > 0) -> integer > 0`
 
 Return a number following a poisson distribution of the provided `mean`
+
+### Perlin noise
+
+#### `tlsd.random.perlin_noise.create() -> perlin_noise`
+
+Create a new perlin noise generator.
+
+It contains no coefficient at startup, they must be added one by one later.
+
+#### `perlin_noise:set_coefficient(posX: integer, posY: integer, coefX: number, coefY: number)`
+
+Add a the perlin coefficient `(coefX, coefY)` at position `(posX, posY)`.
+
+#### `perlin_noise.compute_at(posX: number, posY: number) -> number`
+
+Compute the Perlin noise value at position `(posX, posY)`.
+
+If the coefficients around this position are not set, an error is
+triggered.
--- src/Lua/Api/Random.cpp
+++ src/Lua/Api/Random.cpp
@@ -20,10 +20,10 @@
 #include <iostream>
 
 #include "Random.h"
+#include "../../Random/PerlinNoise.h"
 
 using Random::NumberGenerator;
-
-static const char* NUMBER_GENERATOR_METATABLE = "Tlsd.random.number_generator";
+using Random::PerlinNoise;
 
 // Utility functions
 
@@ -38,7 +38,9 @@ static int returnSeed(lua_State* L)
     return 1;
 }
 
-// Number generator creation
+// Number generator
+
+static const char* NUMBER_GENERATOR_METATABLE = "Tlsd.random.number_generator";
 
 static int createNumberGenerator(lua_State* L)
 {
@@ -79,6 +81,69 @@ static int getNumberGeneratorPoisson(lua_State* L)
     return 1;
 }
 
+// Perlin noise
+
+static const char* PERLIN_NOISE_METATABLE = "Tlsd.random;perlin_noise";
+
+static int createPerlinNoise(lua_State* L)
+{
+    PerlinNoise** noise = static_cast<PerlinNoise**>(lua_newuserdata(L, sizeof(PerlinNoise*)));
+    *noise = new PerlinNoise();
+    luaL_getmetatable(L, PERLIN_NOISE_METATABLE);
+    lua_setmetatable(L, -2);
+    return 1;
+}
+
+static PerlinNoise& getPerlinNoiseArg(lua_State* L, int narg = 1)
+{
+    return **static_cast<PerlinNoise**>(luaL_checkudata(L, narg, PERLIN_NOISE_METATABLE));
+}
+
+static int destroyPerlinNoise(lua_State* L)
+{
+    delete *static_cast<PerlinNoise**>(luaL_checkudata(L, 1, PERLIN_NOISE_METATABLE));
+    return 0;
+}
+
+static int setPerlinNoiseCoeff(lua_State* L)
+{
+    PerlinNoise& noise = getPerlinNoiseArg(L);
+    int posX = luaL_checkint(L, 2);
+    int posY = luaL_checkint(L, 3);
+    float coeffX = luaL_checknumber(L, 4);
+    float coeffY = luaL_checknumber(L, 5);
+
+    noise.setCoefficient(
+        Geometry::Vec2Di(posX, posY),
+        Geometry::Vec2Df(coeffX, coeffY)
+    );
+
+    return 0;
+}
+
+static int computePerlinNoiseAt(lua_State* L)
+{
+    PerlinNoise& noise = getPerlinNoiseArg(L);
+    float posX = luaL_checknumber(L, 2);
+    float posY = luaL_checknumber(L, 3);
+
+    int result = 1;
+
+    try
+    {
+        lua_pushnumber(L, noise.computeAt(Geometry::Vec2Df(posX, posY)));
+
+    }
+    catch (const PerlinNoise::UninitializedCoefficientsException& e)
+    {
+        luaL_error(L, e.what());
+        result = 0;
+    }
+
+    return result;
+}
+
+
 // Setup function
 
 static const struct luaL_Reg randomLib [] = {
@@ -97,18 +162,60 @@ static const struct luaL_Reg randomGenObjLib [] = {
     {NULL, NULL}
 };
 
+static const struct luaL_Reg perlinNoiseLib [] = {
+    {"new", createPerlinNoise},
+    {NULL, NULL}
+};
+
+static const struct luaL_Reg perlinNoiseObjLib [] = {
+    {"set_coefficient", setPerlinNoiseCoeff},
+    {"compute_at", computePerlinNoiseAt},
+    {"__gc", destroyPerlinNoise},
+    {NULL, NULL}
+};
+
+static void setupNumberGenerator(lua_State* L, const ::Random::Seed& seed)
+{
+    // Creating number generator metatable
+    luaL_newmetatable(L, NUMBER_GENERATOR_METATABLE);
+
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
+
+    luaL_setfuncs(L, randomGenObjLib, 0);
+
+    // Creating number generator lib
+    luaL_newlibtable(L, randomGenLib);
+
+    auto luaSeed = (::Random::Seed*)lua_newuserdata(L, sizeof(seed));
+    *luaSeed = seed;
+
+    luaL_setfuncs(L, randomGenLib, 1);
+    lua_setfield(L, -2, "number_generator");
+}
+
+static void setupPerlinNoise(lua_State* L)
+{
+    // Creating Perlin noise metatable
+    luaL_newmetatable(L, PERLIN_NOISE_METATABLE);
+
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
+
+    luaL_setfuncs(L, perlinNoiseObjLib, 0);
+    lua_pop(L, 1);
+
+    // Creating perlin noise lib
+    luaL_newlibtable(L, perlinNoiseLib);
+
+    luaL_setfuncs(L, perlinNoiseLib, 0);
+    lua_setfield(L, -2, "perlin_noise");
+}
+
 void Lua::Api::Random::setup(Lua::Vm& vm, const ::Random::Seed& seed)
 {
     vm.doWithState(
         [seed](lua_State*const L) {
-            // Creating number generator metatable
-            luaL_newmetatable(L, NUMBER_GENERATOR_METATABLE);
-
-            lua_pushvalue(L, -1);
-            lua_setfield(L, -2, "__index");
-
-            luaL_setfuncs(L, randomGenObjLib, 0);
-
             // Creating random lib
             luaL_newlibtable(L, randomLib);
 
@@ -117,14 +224,8 @@ void Lua::Api::Random::setup(Lua::Vm& vm, const ::Random::Seed& seed)
 
             luaL_setfuncs(L, randomLib, 1);
 
-            // Creating number generator lib
-            luaL_newlibtable(L, randomGenLib);
-
-            luaSeed = (::Random::Seed*)lua_newuserdata(L, sizeof(seed));
-            *luaSeed = seed;
-
-            luaL_setfuncs(L, randomGenLib, 1);
-            lua_setfield(L, -2, "number_generator");
+            setupNumberGenerator(L, seed);
+            setupPerlinNoise(L);
 
             // Registering lib
             lua_getglobal(L, "tlsd");
